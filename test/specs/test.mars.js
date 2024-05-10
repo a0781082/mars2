02ffc9827fb0c84d188d6851d3e7d1fa1bddd2fc
Andy Wyatt <tubazilla@gmail.com>
2024-05-10 15:38:41 +0100

    initial commit

---
+++ test/specs/test.mars.js
@@ -0,0 +1,23 @@
+import { expect, browser, $ } from '@wdio/globals'
+import { strictEqual } from 'assert'
+import AxeBuilder from '@axe-core/webdriverio'
+
+describe('Deque Candidate Automation Tests', () => {
+    it('load the Deque Mars page in my browser', async () => {
+        await browser.url(`https://dequeuniversity.com/demo/mars`)
+
+        const mainNav = await browser.$('#main-nav'); 
+        const isDisplayed = await mainNav.isDisplayed();
+
+        strictEqual(isDisplayed, true, 'Main navigation is not loaded');
+    })
+
+    it('and gather the accessibility scan results', async () => {
+        const builder = new AxeBuilder({ client: browser })
+        await browser.url(`https://dequeuniversity.com/demo/mars`)
+
+        const result = await builder.analyze()
+        console.log('Acessibility Results:', result)
+    })
+
+})
